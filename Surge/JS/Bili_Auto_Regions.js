75224a283f7c2c9d069ec75021454aab4e311011
shelken <shelken.pxk@gmail.com>
2022-04-05 21:20:16 +0800

    修改404时切换逻辑

--- Surge/JS/Bili_Auto_Regions.js
+++ Surge/JS/Bili_Auto_Regions.js
@@ -80,7 +80,7 @@ async function SwitchRegion(play) {
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || 'BiliTW'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || 'BiliHK'; //Your HongKong sub-policy name.
-	const DF = $.read('BiliArea_DF') || 'DIRECT'; //Sub-policy name used after region is blocked(e.g. url 404)
+	const DF = $.read('BiliArea_DF') || 'BiliHK'; //Sub-policy name used after region is blocked(e.g. url 404)
 	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
